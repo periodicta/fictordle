13e71f720e8fcdc6e0be9b007c9b267935646a7d
periodicta <87677587+periodicta@users.noreply.github.com>
2022-03-12 16:42:07 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -1,19 +1,1 @@
-#!/usr/bin/env sh
 
-# abort on errors
-set -e
-
-# build
-npm run build
-
-# navigate into the build output directory
-cd dist
-
-git init
-git add -A
-git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:periodicta/vue-wordle.git master:gh-pages
-
-cd -
