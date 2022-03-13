be92f60288bb03a4ce063788dfaeea0a3d8dd546
periodicta <87677587+periodicta@users.noreply.github.com>
2022-03-12 21:46:46 -0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -14,6 +14,6 @@ git add -A
 git commit -m 'deploy'
 
 # if you are deploying to https://<USERNAME>.github.io/<REPO>
-git push -f git@github.com:periodicta/vue-wordle.git master:gh-pages
+git push -f git@github.com:periodicta/fictordle.git master:gh-pages
 
 cd -
